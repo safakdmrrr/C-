966bf0c62197e252b1e92c5af58c6bd7d773485a
safakdmrrr <safak1536@gmail.com>
2022-11-05 00:50:34 +0300

    5.11.22

---
+++ ŞafakDemir_Ödevler/carpma/carpma/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carpma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i < 10;)
+            {
+                i++;
+                Console.WriteLine(i+ " ile çarpım tablosu*******");
+                for (int j = 0; j <11; )
+                {
+                 
+                    Console.WriteLine(i+"*"+j+"="+(i*j));
+                    
+                    j++;
+                }
+
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
